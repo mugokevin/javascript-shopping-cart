33d19e4f18607b6a67d6c6a57997f8e1b4a640e2
mugokevin <kevinmugo4@gmail.com>
2022-07-08 10:39:13 +0300

    initial commit

---
+++ src/cart.js
@@ -0,0 +1,265 @@
+
+// let shopItemsData = [{
+//     id:  "aaa",
+//     name:"Casual Shirt",
+//     price: 45,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-1.jpg"
+// },
+// {
+//     id:"bbb",
+//     name:"Office Shirt",
+//     price: 100,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-2.jpg"
+// },
+// {
+//     id: "ccc",
+//     name:"T-Shirt",
+//     price: 25,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-3.jpg"
+// },
+// {
+//     id: "ddd",
+//     name:"Mens suit",
+//     price: 300,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-4.jpg"
+// }];
+
+let shopItemsData = [
+    {
+      id: "aaa",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "bbb",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "ccc",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "ddd",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+    {
+      id: "eee",
+      name: "Mens Tie",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-5.png",
+    },
+    {
+      id: "fff",
+      name: "Casual shoes",
+      price: 200,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-6.png",
+    },
+    {
+      id: "ggg",
+      name: "black suit",
+      price: 450,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-7.png",
+    },
+    {
+      id: "hhh",
+      name: "polo shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-8.png",
+    },
+    {
+      id: "iii",
+      name: "denim shirt",
+      price: 85,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-9.png",
+    },
+    {
+      id: "jjj",
+      name: "denim pants",
+      price: 120,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-10.png",
+    },
+    {
+      id: "kkk",
+      name: "basic cap",
+      price: 35,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-11.png",
+    },
+    {
+      id: "lll",
+      name: "leather boots",
+      price: 350,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-12.png",
+    },
+  ];
+  
+  
+
+let label = document.getElementById('label');
+let shoppingCart = document.getElementById('shopping-cart');
+
+
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () =>{
+    let cartIcon = document.getElementById("cartAmount");
+    cartIcon.innerHTML = basket.map((x)=> x.item).reduce((x,y)=> x+y,0);
+}
+calculation();
+
+let generateCartItems = () =>{
+    if(basket.length !== 0){
+        return (shoppingCart.innerHTML = basket.map((x)=>{
+            let { id, item } = x;
+            let search = shopItemsData.find((y)=>y.id === id) || [];
+            // return `
+            // <div class="cart-item">
+            // <img width="100" src= ${search.img} alt="">
+            // </div>
+            // `
+            return `
+            <div class="cart-item">
+                  <img width="100" src= ${ search.img} alt="">
+              <div class="details">
+        
+                <div class="title-price-x">
+                    <h4 class="title-price"> 
+                         <p>${ search.name}</p>
+                         <p class="cart-item-price">$ ${ search.price}</p>
+                     </h4>
+                     <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+                </div>
+        
+                   <div class="buttons">
+                     <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+                     <div id=${id} class="quantity">${item}</div>
+                     <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+                   </div>
+        
+                 <h3>$ ${item * search.price}</h3>
+             </div>
+            </div>
+            `;
+        }).join(""));
+    }
+    else{
+        shoppingCart.innerHTML = ``;
+        label.innerHTML = `
+         <h2>Cart is Empty</h2>
+         <a href="index.html">
+           <button class="HomeBtn">Back To Home</button> 
+         </a>
+        `;
+    }
+};
+generateCartItems();
+
+
+
+let increment = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=> x.id === selectedItem.id);
+
+    if(search === undefined){
+        basket.push({
+            id: selectedItem.id,
+            item: 1,
+        });
+    }
+    else{
+        search.item +=1;
+    }
+    generateCartItems();
+    update(selectedItem.id);
+    localStorage.setItem("data",JSON.stringify(basket));
+
+};
+
+let decrement = (id)=>{
+    let selectedItem = id;
+    let search = basket.find((x)=>x.id === selectedItem.id);
+
+    if(search === undefined)return;
+    else if(search.item === 0) return;
+    else{
+        search.item -=1;
+    }
+
+    update(selectedItem.id);
+
+    basket = basket.filter((x)=> x.item !==0);
+
+    generateCartItems();
+   
+    localStorage.setItem("data",JSON.stringify(basket));
+
+};
+
+
+
+let update = (id)=>{
+    let search = basket.find((x)=> x.id === id );
+    // console.log(search.item);
+    document.getElementById(id).innerHTML = search.item;
+    calculation();
+    TotalAmount();
+};
+
+let removeItem = (id) => {
+    let selectedItem = id;
+    basket = basket.filter((x)=>x.id !== selectedItem.id);
+    generateCartItems();
+    TotalAmount();
+    calculation();
+    localStorage.setItem("data",JSON.stringify(basket));
+}
+
+let clearCart = () => {
+    basket = [];
+    generateCartItems();
+    calculation();
+    localStorage.setItem("data",JSON.stringify(basket));
+
+}
+
+let TotalAmount = () => {
+    if(basket.length !==0){
+        let amount = basket.map((x)=>{
+            let {item, id} = x
+            let search = shopItemsData.find((y)=>y.id === id) || [];
+            return item * search.price
+
+        }).reduce((x,y)=> x+y,0)
+        // console.log(amount)
+        label.innerHTML = `
+        <h2>Total Bill: $ ${amount}</h2>
+        <button class="checkout">Checkout</button>
+        <button onclick="clearCart()" class="removeAll">Clear cart</button>
+        `
+    }
+    
+    else return
+}
+TotalAmount();
---
+++ src/data.js
@@ -0,0 +1,117 @@
+
+// let shopItemsData = [{
+//     id:  "aaa",
+//     name:"Casual Shirt",
+//     price: 45,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-1.jpg"
+// },
+// {
+//     id:"bbb",
+//     name:"Office Shirt",
+//     price: 100,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-2.jpg"
+// },
+// {
+//     id: "ccc",
+//     name:"T-Shirt",
+//     price: 25,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-3.jpg"
+// },
+// {
+//     id: "ddd",
+//     name:"Mens suit",
+//     price: 300,
+//     desc:"Lorem ipsum dolor sit, amet consectetur adipisicing.",
+//     img:"images/img-4.jpg"
+// }];
+
+let shopItemsData = [
+    {
+      id: "aaa",
+      name: "Casual Shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-1.jpg",
+    },
+    {
+      id: "bbb",
+      name: "Office Shirt",
+      price: 100,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-2.jpg",
+    },
+    {
+      id: "ccc",
+      name: "T Shirt",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-3.jpg",
+    },
+    {
+      id: "ddd",
+      name: "Mens Suit",
+      price: 300,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-4.jpg",
+    },
+    {
+      id: "eee",
+      name: "Mens Tie",
+      price: 25,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-5.png",
+    },
+    {
+      id: "fff",
+      name: "Casual shoes",
+      price: 200,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-6.png",
+    },
+    {
+      id: "ggg",
+      name: "black suit",
+      price: 450,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-7.png",
+    },
+    {
+      id: "hhh",
+      name: "polo shirt",
+      price: 45,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-8.png",
+    },
+    {
+      id: "iii",
+      name: "denim shirt",
+      price: 85,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-9.png",
+    },
+    {
+      id: "jjj",
+      name: "denim pants",
+      price: 120,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-10.png",
+    },
+    {
+      id: "kkk",
+      name: "basic cap",
+      price: 35,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-11.png",
+    },
+    {
+      id: "lll",
+      name: "leather boots",
+      price: 350,
+      desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+      img: "images/img-12.png",
+    },
+  ];
+  
